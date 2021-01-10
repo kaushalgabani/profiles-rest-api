16095c5bb0e06c7c3de867e445595f6ced73ab3e
kaushagabani <kaushalgabani21@gmail.com>
2021-01-10 19:55:03 +0530

    commit

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -2,9 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers
+from django.shortcuts import render
+from profiles_api import serializers, models, permissions
 
 
 class HelloAPiView(APIView):
@@ -84,3 +85,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy (self, request, pk=None):
         """Handle removing an object"""
         return Response({'https_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
